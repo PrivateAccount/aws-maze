0ca3f6d87dc0ef7406b9f6c6cbae28f3c35fc1c4
Andrzej <andrzuk@gmail.com>
2023-08-29 11:36:30 +0200

    Display steps counter;

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aws-maze
 Simple maze demo deployed on AWS Amplify.
+## Features:
+* Stack: HTML, CSS, JavaScript
+* Platform: Amazon Web Services
+* Maze files: https://github.com/micromouseonline/micromouse_maze_tool/tree/master/mazefiles
+* On-line: https://main.d33gh15xw1gyqq.amplifyapp.com
--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.onload = function() {
 
 	const direction = { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3 };
 	const state = { STOP: 0, START: 1 };
-    const finishRange = { LEFT: 28, RIGHT: 36, TOP: 14, BOTTOM: 18 };
+	const finishRange = { LEFT: 28, RIGHT: 36, TOP: 14, BOTTOM: 18 };
 
 	const mapsControl = document.getElementById('maze-select');
 
@@ -36,10 +36,11 @@ window.onload = function() {
 		markVisited = markHistory.checked;
 	});
 
-    const displayPosX = document.getElementById('position-x');
-    const displayPosY = document.getElementById('position-y');
-    const displayTimeMin = document.getElementById('time-min');
-    const displayTimeSec = document.getElementById('time-sec');
+	const displayStep = document.getElementById('step');
+	const displayPosX = document.getElementById('position-x');
+	const displayPosY = document.getElementById('position-y');
+	const displayTimeMin = document.getElementById('time-min');
+	const displayTimeSec = document.getElementById('time-sec');
 
 	stopButton.disabled = true;
 	
@@ -110,16 +111,16 @@ window.onload = function() {
 		y: 0,
 		direction: direction.RIGHT,
 		state: state.STOP,
-        step: 0,
+		step: 0,
 		color: '#c00',
-        finish: '#090',
+		finish: '#090',
 		init: function() {
 			this.x = 1;
 			this.y = 1;
-            this.step = 0;
+			this.step = 0;
 			mazeArea.paintCell(this.x, this.y, this.color);
-            this.updateDisplay();
-            startButton.disabled = false;
+			this.updateDisplay();
+			startButton.disabled = false;
 		},
 		isCollision: function() {
 			var index = 0;
@@ -209,86 +210,86 @@ window.onload = function() {
 			if (this.isNewer(mazeCount[index]).found) {
 				this.direction = this.isNewer(mazeCount[index]).direction;
 			}
-            var which = 0, newIndex = 0, found = false;
-            do {
-                which = Math.floor(Math.random() * 2);
-                switch (this.direction) {
-                    case direction.UP:
-                        newIndex = (this.y - 1) * mazeArea.colsCount + this.x;
-                        if (this.isCollision()) { 
-                            this.direction = which ? direction.LEFT : direction.RIGHT; 
-                        }
-                        else if (this.isBranchLeft()) { 
-                            this.direction = direction.LEFT; 
-                            found = true;
-                        }
-                        else if (this.isBranchRight()) { 
-                            this.direction = direction.RIGHT; 
-                            found = true;
-                        }
-                        else if (mazeMap[newIndex]) { this.y--; found = true; }
-                        else this.direction = which ? direction.LEFT : direction.RIGHT;
-                        break;
-                    case direction.DOWN:
-                        newIndex = (this.y + 1) * mazeArea.colsCount + this.x;
-                        if (this.isCollision()) { 
-                            this.direction = which ? direction.LEFT : direction.RIGHT; 
-                        }
-                        else if (this.isBranchLeft()) { 
-                            this.direction = direction.LEFT; 
-                            found = true;
-                        }
-                        else if (this.isBranchRight()) { 
-                            this.direction = direction.RIGHT; 
-                            found = true;
-                        }
-                        else if (mazeMap[newIndex]) { this.y++; found = true; }
-                        else this.direction = which ? direction.LEFT : direction.RIGHT;
-                        break;
-                    case direction.LEFT:
-                        newIndex = this.y * mazeArea.colsCount + this.x - 1;
-                        if (this.isCollision()) { 
-                            this.direction = which ? direction.UP : direction.DOWN; 
-                        }
-                        else if (this.isBranchLeft()) { 
-                            this.direction = direction.DOWN; 
-                            found = true;
-                        }
-                        else if (this.isBranchRight()) { 
-                            this.direction = direction.UP; 
-                            found = true;
-                        }
-                        else if (mazeMap[newIndex]) { this.x--; found = true; }
-                        else this.direction = which ? direction.UP : direction.DOWN;
-                        break;
-                    case direction.RIGHT:
-                        newIndex = this.y * mazeArea.colsCount + this.x + 1;
-                        if (this.isCollision()) { 
-                            this.direction = which ? direction.UP : direction.DOWN; 
-                        }
-                        else if (this.isBranchLeft()) { 
-                            this.direction = direction.UP; 
-                            found = true;
-                        }
-                        else if (this.isBranchRight()) { 
-                            this.direction = direction.DOWN; 
-                            found = true;
-                        }
-                        else if (mazeMap[newIndex]) { this.x++; found = true; }
-                        else this.direction = which ? direction.UP : direction.DOWN;
-                        break;
-                }
-            }
-            while (!found);
+			var which = 0, newIndex = 0, found = false;
+			do {
+				which = Math.floor(Math.random() * 2);
+				switch (this.direction) {
+					case direction.UP:
+						newIndex = (this.y - 1) * mazeArea.colsCount + this.x;
+						if (this.isCollision()) { 
+							this.direction = which ? direction.LEFT : direction.RIGHT; 
+						}
+						else if (this.isBranchLeft()) { 
+							this.direction = direction.LEFT; 
+							found = true;
+						}
+						else if (this.isBranchRight()) { 
+							this.direction = direction.RIGHT; 
+							found = true;
+						}
+						else if (mazeMap[newIndex]) { this.y--; found = true; }
+						else this.direction = which ? direction.LEFT : direction.RIGHT;
+						break;
+					case direction.DOWN:
+						newIndex = (this.y + 1) * mazeArea.colsCount + this.x;
+						if (this.isCollision()) { 
+							this.direction = which ? direction.LEFT : direction.RIGHT; 
+						}
+						else if (this.isBranchLeft()) { 
+							this.direction = direction.LEFT; 
+							found = true;
+						}
+						else if (this.isBranchRight()) { 
+							this.direction = direction.RIGHT; 
+							found = true;
+						}
+						else if (mazeMap[newIndex]) { this.y++; found = true; }
+						else this.direction = which ? direction.LEFT : direction.RIGHT;
+						break;
+					case direction.LEFT:
+						newIndex = this.y * mazeArea.colsCount + this.x - 1;
+						if (this.isCollision()) { 
+							this.direction = which ? direction.UP : direction.DOWN; 
+						}
+						else if (this.isBranchLeft()) { 
+							this.direction = direction.DOWN; 
+							found = true;
+						}
+						else if (this.isBranchRight()) { 
+							this.direction = direction.UP; 
+							found = true;
+						}
+						else if (mazeMap[newIndex]) { this.x--; found = true; }
+						else this.direction = which ? direction.UP : direction.DOWN;
+						break;
+					case direction.RIGHT:
+						newIndex = this.y * mazeArea.colsCount + this.x + 1;
+						if (this.isCollision()) { 
+							this.direction = which ? direction.UP : direction.DOWN; 
+						}
+						else if (this.isBranchLeft()) { 
+							this.direction = direction.UP; 
+							found = true;
+						}
+						else if (this.isBranchRight()) { 
+							this.direction = direction.DOWN; 
+							found = true;
+						}
+						else if (mazeMap[newIndex]) { this.x++; found = true; }
+						else this.direction = which ? direction.UP : direction.DOWN;
+						break;
+				}
+			}
+			while (!found);
 			mazeArea.paintCell(this.x, this.y, this.color);
-            this.checkIsFinish();
-            this.updateDisplay();
+			this.checkIsFinish();
+			this.updateDisplay();
 			if (this.state == state.START) {
 				setTimeout(function() {
 					mazeMouse.move();
 				}, MOVE_STEP_PERIOD);
 			}
-            this.step++;
+			this.step++;
 		},
 		start: function() {
 			startButton.disabled = true;
@@ -303,33 +304,34 @@ window.onload = function() {
 			mapsControl.disabled = false;
 			this.state = state.STOP;
 		},
-        checkIsFinish: function() {
-            if (this.x > finishRange.LEFT && this.x < finishRange.RIGHT && this.y > finishRange.TOP && this.y < finishRange.BOTTOM) {
-                mazeArea.paintCell(this.x - 1, this.y - 1, this.finish);
-                mazeArea.paintCell(this.x + 1, this.y - 1, this.finish);
-                mazeArea.paintCell(this.x, this.y - 1, this.finish);
-                mazeArea.paintCell(this.x - 1, this.y, this.finish);
-                mazeArea.paintCell(this.x + 1, this.y, this.finish);
-                mazeArea.paintCell(this.x - 1, this.y + 1, this.finish);
-                mazeArea.paintCell(this.x + 1, this.y + 1, this.finish);
-                mazeArea.paintCell(this.x, this.y + 1, this.finish);
-                this.stop();
-                startButton.disabled = true;
-            }
-        },
-        updateDisplay: function() {
-            const dt = 1000 / MOVE_STEP_PERIOD;
-            var minute = 0, second = 0, ticks = 0;
-            displayPosX.innerText = this.x;
-            displayPosY.innerText = this.y;
-            if (this.step % dt == 0) {
-                ticks = this.step / dt;
-                second = Math.floor(ticks % 60);
-                minute = Math.floor(ticks / 60);
-                displayTimeMin.innerText = minute.toString();
-                displayTimeSec.innerText = second.toString();
-            }
-        },
+		checkIsFinish: function() {
+			if (this.x > finishRange.LEFT && this.x < finishRange.RIGHT && this.y > finishRange.TOP && this.y < finishRange.BOTTOM) {
+				mazeArea.paintCell(this.x - 1, this.y - 1, this.finish);
+				mazeArea.paintCell(this.x + 1, this.y - 1, this.finish);
+				mazeArea.paintCell(this.x, this.y - 1, this.finish);
+				mazeArea.paintCell(this.x - 1, this.y, this.finish);
+				mazeArea.paintCell(this.x + 1, this.y, this.finish);
+				mazeArea.paintCell(this.x - 1, this.y + 1, this.finish);
+				mazeArea.paintCell(this.x + 1, this.y + 1, this.finish);
+				mazeArea.paintCell(this.x, this.y + 1, this.finish);
+				this.stop();
+				startButton.disabled = true;
+			}
+		},
+		updateDisplay: function() {
+			const dt = 1000 / MOVE_STEP_PERIOD;
+			var minute = 0, second = 0, ticks = 0;
+			displayStep.innerText = this.step.toString();
+			displayPosX.innerText = this.x;
+			displayPosY.innerText = this.y;
+			if (this.step % dt == 0) {
+				ticks = this.step / dt;
+				second = Math.floor(ticks % 60);
+				minute = Math.floor(ticks / 60);
+				displayTimeMin.innerText = minute.toString();
+				displayTimeSec.innerText = second.toString();
+			}
+		},
 	};
 
 	mazeArea.init();
